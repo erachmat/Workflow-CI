bc6e5c6b5df01106b6031dc92e7b7f6ad7f4c0a8
erachmat <ekorahmat.dev@gmail.com>
2026-07-04 10:55:52 +0000

    Criteria 3 Advance: Added MLProject, Conda env, and GitHub Actions Docker pipeline

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,27 +7,27 @@ import shutil
 
 def train_baseline():
     mlflow.autolog()
-    
+
     print("Loading preprocessed data...")
     train_df = pd.read_csv('telco_churn_preprocessing/train.csv')
     test_df = pd.read_csv('telco_churn_preprocessing/test.csv')
-    
+
     X_train = train_df.drop(columns=['Churn'])
     y_train = train_df['Churn']
     X_test = test_df.drop(columns=['Churn'])
     y_test = test_df['Churn']
-    
+
     with mlflow.start_run(run_name="ci_cd_training"):
         print("Training Random Forest in MLflow Project...")
         model = RandomForestClassifier(random_state=42)
         model.fit(X_train, y_train)
-        
+
         score = model.score(X_test, y_test)
         print(f"Test Accuracy: {score:.4f}")
-        
+
         if os.path.exists("saved_model"):
             shutil.rmtree("saved_model")
-            
+
         # FIX: Explicitly enforce Python 3.10.12 to bypass the get-pip.py Python 3.9 bug
         custom_env = {
             "name": "telco_env",
@@ -45,7 +45,7 @@ def train_baseline():
                 }
             ]
         }
-        
+
         mlflow.sklearn.save_model(model, "saved_model", conda_env=custom_env)
         print("Model saved locally to 'saved_model' directory.")
 
